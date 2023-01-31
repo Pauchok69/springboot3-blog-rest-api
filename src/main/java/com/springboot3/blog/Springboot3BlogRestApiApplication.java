d253601f8d36027f064353a375e8ce7c85f1266c
Andy <andrew.zerko@gmail.com>
2023-01-31 21:05:45 +0200

    Initial commit

---
+++ src/main/java/com/springboot3/blog/Springboot3BlogRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.springboot3.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3BlogRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3BlogRestApiApplication.class, args);
+	}
+
+}
