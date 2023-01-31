49690edc412decf519528a4000bf82bd5913bdff
Andy <andrew.zerko@gmail.com>
2023-02-01 00:08:25 +0200

    Add Post entity

--- src/main/java/com/springboot3/blog/Springboot3BlogRestApiApplication.java
+++ src/main/java/com/springboot3/blog/Springboot3BlogRestApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Springboot3BlogRestApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Springboot3BlogRestApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot3BlogRestApiApplication.class, args);
+    }
 
 }
--- src/test/java/com/springboot3/blog/Springboot3BlogRestApiApplicationTests.java
+++ src/test/java/com/springboot3/blog/Springboot3BlogRestApiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Springboot3BlogRestApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
